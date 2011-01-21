aa1addcb6bf51756b68ba46f1e3057d9dd7c7694
Matt McCormick (thewtex) <matt@mmmccormick.com>
2011-01-21 16:11:22 -0600

    Save interp method figures.

---
+++ analysis/common/mpl_rc_settings.py
@@ -0,0 +1,4 @@
+import matplotlib as mpl
+mpl.rcParams['lines.linewidth'] = 1.8
+mpl.rcParams['savefig.dpi'] = 200
+
--- analysis/uniform/simulation/interp_method/plot_results.py
+++ analysis/uniform/simulation/interp_method/plot_results.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+import os
 import sys
 
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import pylab
 
+sys.path.insert( 0, os.path.abspath( os.path.join( __file__, '..', '..', '..', '..' )))
+import common.mpl_rc_settings as settings
 
 class PlotResults( object ):
 
@@ -37,6 +40,7 @@ class PlotResults( object ):
         index_is_reg = self.r['regularization'] == reg
         strains = list( set( self.r['strain_percent'] ) )
         strains.sort()
+        strains = strains[:5]
         s = np.ones( len( strains )) * 10**-5
         m = np.ones( len( strains )) * 10**-5
         e = np.ones( len( strains )) * 10**-5
@@ -62,18 +66,32 @@ class PlotResults( object ):
         plt.legend( loc='best' )
         if self.logplot:
             plt.gca().set_yscale( 'log' )
-        plt.ylim( (10**-2, 10**2 ) )
+        plt.ylim( (10**-1.2, 10**1.5 ) )
+        plt.xlim( (0.0, s[-1]+0.5) )
         #plt.ylim( (0.0, 6.0 ) )
 
 plot_results = PlotResults()
-curves = dict( noInterp_sim = 'No Interpolation',
-        cosine_sim = 'Cosine',
-        parabolic_sim = 'Parabolic',
-        sinc_sim = 'Sinc - Amoeba',
-        gradientDescent_sim = 'Sinc - Gradient Descent' )
-#curves = dict( gradientDescent = 'Sinc - Gradient Descent' )
-for curve, curve_name in curves.iteritems():
+curves = [ ('noInterp_sim',  'No Interpolation'),
+        ('cosine_sim',  'Cosine'),
+        ('parabolic_sim', 'Parabolic'),
+        ('sinc_sim', 'Sinc - Amoeba'),
+        ('gradientDescent_sim', 'Sinc - Gradient Descent') ]
+for curve, curve_name in curves:
     r = pylab.csv2rec( 'SNRe_' + curve + '.csv' )
     plot_results.plot_snre_curves( curve, curve_name, r )
+
+
+plt.figure( 1 )
+plt.savefig( '../../../../doc/images/interp_method_simulation_regularization_axial.png' )
+plt.savefig( '../../../../doc/images/interp_method_simulation_regularization_axial.eps' )
+plt.figure( 2 )
+plt.savefig( '../../../../doc/images/interp_method_simulation_regularization_lateral.png' )
+plt.savefig( '../../../../doc/images/interp_method_simulation_regularization_lateral.eps' )
+plt.figure( 3 )
+plt.savefig( '../../../../doc/images/interp_method_simulation_no_regularization_axial.png' )
+plt.savefig( '../../../../doc/images/interp_method_simulation_no_regularization_axial.eps' )
+plt.figure( 4 )
+plt.savefig( '../../../../doc/images/interp_method_simulation_no_regularization_lateral.png' )
+plt.savefig( '../../../../doc/images/interp_method_simulation_no_regularization_lateral.eps' )
 
 plt.show()
