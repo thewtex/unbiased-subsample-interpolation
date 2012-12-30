42203065a98a82bfc482653d8ac3f20db1309761
Matt McCormick <matt.mccormick@kitware.com>
2012-12-29 19:14:28 -0500

    Cosine fit diagram.

--- analysis/interpolation_diagram/interpolation_diagram.py
+++ analysis/interpolation_diagram/interpolation_diagram.py
@@ -24,7 +24,7 @@ ax.set_xlabel('Displacement')
 #ax.set_ylabel('XCORR')
 ax.spines['right'].set_color('none')
 ax.spines['top'].set_color('none')
-samples_x = np.array([7.9, 9.4, 10.9])
+samples_x = np.array([8.0, 9.5, 11.0])
 samples_y = j1(samples_x)
 ax.plot(samples_x, j1(samples_x), 'o')
 ax.set_ylim([-0.3, 0.4])
@@ -34,8 +34,9 @@ def parabolic_interpolation(y0, y1, y2, T_s):
     denominator = 2.0*(y0 - 2*y1 + y2)
     return T_s * numerator / denominator
 
+T_s = samples_x[1] - samples_x[0]
 parabolic_peak = samples_x[1] + parabolic_interpolation(samples_y[0],
-    samples_y[1], samples_y[2], samples_x[1] - samples_x[0])
+    samples_y[1], samples_y[2], T_s)
 
 aa = np.array([samples_x**2, samples_x, np.ones((3,))])
 aa = aa.transpose()
@@ -53,6 +54,34 @@ ax.vlines([parabolic_peak, true_peak], bias_ylim[0], bias_ylim[1])
 ax.annotate('bias', xy=(true_peak, 0.1), xytext=(true_peak+0.3, 0.0),
     arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.2'))
 
+ax = plt.subplot(222)
+ax.plot(xx, yy, label='XCORR')
+ax.plot([true_peak], [yy.max()], 'p')
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_xlabel('Displacement')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.plot(samples_x, j1(samples_x), 'o')
+ax.set_ylim([-0.3, 0.4])
+
+def cosine_fit(y0, y1, y2):
+    omega = np.arccos((y0+y2)/(2*y1))
+    theta = np.arctan((y0-y2)/(2*y1*np.sin(omega)))
+    delta = -theta/omega
+    return delta, omega, theta
+
+delta, omega, theta = cosine_fit(samples_y[0], samples_y[1], samples_y[2])
+cosine_peak = samples_x[1] + T_s*delta
+ax.plot(cosine_peak, j1(cosine_peak), '*')
+ax.plot(xx, yy.max()*np.cos(((xx-samples_x[1])/T_s)*omega + theta), '--', label='cosine fit')
+ax.legend(loc='lower left')
+
+ax.vlines([cosine_peak, true_peak], bias_ylim[0], bias_ylim[1])
+
+ax.annotate('bias', xy=(true_peak, 0.1), xytext=(true_peak+0.3, 0.0),
+    arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=0.2'))
+
 plt.show()
-
-
